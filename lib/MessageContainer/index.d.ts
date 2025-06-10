077806fa013acf8785174a92f25b8e30cd8e9531
Colton-ProBuiltSoftware <coltonhillebrand92@icloud.com>
2025-06-10 11:52:22 -0500

    update

--- lib/MessageContainer/index.d.ts
+++ lib/MessageContainer/index.d.ts
@@ -2,22 +2,5 @@ import React from 'react';
 import { IMessage } from '../types';
 import { MessageContainerProps } from './types';
 export * from './types';
-export interface RenderCellType {
-    children?: Children;
-    index?: number;
-    onLayout?: any[];
-    style?: Style;
-}
-export interface Children {
-}
-export interface Style {
-    alignItems?: string;
-    flexDirection?: string;
-    left?: number;
-    position?: string;
-    top?: number;
-    transform?: Array<Children[]>;
-    width?: number;
-}
 declare function MessageContainer<TMessage extends IMessage = IMessage>(props: MessageContainerProps<TMessage>): React.JSX.Element;
 export default MessageContainer;
